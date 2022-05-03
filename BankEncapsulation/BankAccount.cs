8803a8db1433451c5c8cc1d3ab50662e6cea4ddf
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-05-03 12:19:01 -0400

    bank encap

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit( double cash )       
+        {
+            balance += cash;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount currentBalance = new BankAccount();
+            
+            Console.WriteLine("How much would you like to deposit today?" );
+
+            var amount = double.Parse(Console.ReadLine());
+
+            currentBalance.Deposit(amount);
+
+            var balance = currentBalance.GetBalance();
+
+            Console.WriteLine(balance);
+
+        }
+    }
+}
